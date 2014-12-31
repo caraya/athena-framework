29882d9cce6976e4a862c3ae6bed2d237eb88a25
Carlos Araya <carlos.araya@gmail.com>
2014-12-30 19:11:09 -0800

    working on serviceworker fetch

--- sw.js
+++ sw.js
@@ -12,29 +12,18 @@ importScripts('js/serviceworker-cache-polyfill.js');
 
 var CACHE_NAME = 'athena-demo';
 var CACHE_VERSION = 3;
-
+debugger;
 self.oninstall = function(event) {
-  console.log('I installed successfully');
   event.waitUntil(
     caches.open(CACHE_NAME + '-v' + CACHE_VERSION).then(function(cache) {
-
       return cache.addAll([
-        // This is where you add the files to cache
-        // web components
         '/bower_components/',
-        // HTML content and component wrappers
         '/content/',
-        // CSS
         '/css/',
-        // Javasacript
         '/js/',
-        // Layout component
         '/layouts/',
-        // Content Pages
         'index.html',
         'notes.html',
-        // Testing whether I can add external content to the serviceworker
-        // to use
         'http://chimera.labs.oreilly.com/books/1230000000345/ch12.html',
         'http://chimera.labs.oreilly.com/books/1230000000345/apa.html'
       ]);
@@ -61,36 +50,11 @@ self.onactivate = function(event) {
 
 };
 
-self.onfetch = function(event) {
-  var request = event.request;
-  var requestURL = new URL(event.request.url);
-
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-
-    // Check the cache for a hit.
-    caches.match(request).then(function(response) {
-
-      // If we have a response return it.
-      if (response)
-        return response;
-
-      // Otherwise fetch it, store and respond.
-      return fetch(request).then(function(response) {
-
-        var responseToCache = response.clone();
-
-        caches.open(CACHE_NAME + '-v' + CACHE_VERSION).then(
-          function(cache) {
-            cache.put(request, responseToCache).catch(function(err) {
-              // Likely we got an opaque response which the polyfill
-              // can't deal with, so log out a warning.
-              console.warn(requestURL + ': ' + err.message);
-            });
-          });
-
-        return response;
-      });
-
+    cachesPolyfill.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+      console.log("got resource" + event.request);
     })
   );
-};
+});
