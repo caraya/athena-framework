9490d3a7d5f044443174f5d48a21f3b1a09c6cfb
Carlos Araya <carlos.araya@gmail.com>
2014-12-30 19:23:24 -0800

    more work on SW

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 
 // This polyfill provides Cache.add(), Cache.addAll(),
 // and CacheStorage.match(), which are not implemented in Chrome 40.
-importScripts('./serviceworker-cache-polyfill.js');
+importScripts('js/serviceworker-cache-polyfill.js');
 // The SW will be shutdown when not in use to save memory,
 // be aware that any global state is likely to disappear
 console.log("SW startup");
